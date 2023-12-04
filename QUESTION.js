de9d6f448af9eabfd977173adf91c8ac26d6041e
FredWantou <freddywantou@hotmail.com>
2023-12-04 17:08:30 +0100

    Developpement des specifications 1 et 2. Lister les questions de la base de donnee et creer un examen et zjouter des questions, mise à jour de l'interface caporal

--- QUESTION.js
+++ QUESTION.js
@@ -5,5 +5,10 @@ var QUESTION = function(nm,ans){
 	//this.type = type;
 }
 
+//Methode tostring retourne la chaine de caractere avec laquelle afficher l'objet question
+QUESTION.prototype.toString = function(){
+	return `question:${this.name}\n reponse:${this.answer}\n\n`;
+}
+
 
 module.exports = QUESTION;
